66cbbfeadaccc27707474cc1433e67894e0ef70f
sureshrobin <124233611+sureshrobin@users.noreply.github.com>
2025-02-11 09:26:41 +0000

    arithmetic,dictionary,elseif added

---
+++ Helpdesk Project/Arithmetic.py
@@ -0,0 +1,13 @@
+a, b = 10, 3
+print(a + b)   # Addition (13)
+print(a - b)   # Subtraction (7)
+print(a * b)   # Multiplication (30)
+print(a / b)   # Division (3.33)
+print(a // b)  # Floor Division (3)
+print(a % b)   # Modulus (1)
+print(a ** b)  # Exponentiation (10^3 = 1000)
+
+#Comparision Operator
+print(a > b)   # True
+print(a == b)  # False
+print(a != b)  # True
